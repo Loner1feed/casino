e383f4a6ea18abc2e5693de094b8dfcfd19e46aa
Loner1feed <45013853+Loner1feed@users.noreply.github.com>
2021-03-29 14:43:37 +0300

    4

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    var note = $('.main__timer'),
+            ts = new Date(2012, 0, 1),
+            newYear = true;
+    if((new Date()) > ts){
+            // The new year is here! Count towards something else.
+            // Notice the *1000 at the end - time must be in milliseconds
+            ts = (new Date()).getTime() + 10*24*60*60*1000;
+            newYear = false;
+    }
+    $('#countdown').countdown({
+            timestamp        : ts,
+            callback        : function(days, hours, minutes, seconds){
+                    var message = "";
+                    message += hours + " часов" + " ";
+                    message += minutes + " минут" + " ";
+                    message += seconds + " секунд";
+                    note.html(message);
+            }
+    });
+});
