eb9ba8cb3f42010f9320fbfb4ae160c27344680a
Loner1feed <45013853+Loner1feed@users.noreply.github.com>
2021-03-31 16:37:32 +0300

    6

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+        $('.preload').addClass('preload--closeAnim');
+        window.setTimeout(function () {
+                $('.preload').removeClass('preload--closeAnim');
+        }, 500);
+})
+
 $(function(){
 
     var note = $('.main__timer'),
@@ -20,3 +27,21 @@ $(function(){
             }
     });
 });
+
+function arrowAnim(name){
+        name.mouseenter(function(){
+                name.children('.animated-arrow').addClass('animated-arrow__anim');
+                window.setTimeout(function () {
+                        $('.animated-arrow__anim').removeClass('animated-arrow__anim');
+                }, 1000);
+                        // alert('hhh');
+        })
+}
+
+steps1 = $('#steps1');
+steps2 = $('#steps2');
+steps3 = $('#steps3');
+
+arrowAnim(steps1);
+arrowAnim(steps2);
+arrowAnim(steps3);
